db2d3eb14529c658dc34c9e37f1eeb7a5c604f2e
Mike Mwangi <mikemwangi0551@gmail.com>
2021-08-31 22:46:42 +0300

    s receiving responses from android app

--- app/Http/Controllers/sms/SendSMSController.php
+++ app/Http/Controllers/sms/SendSMSController.php
@@ -18,13 +18,13 @@ class SendSMSController extends Controller
 
     public function makeHttp($url, $body)
     {
-        $url = 'https://api.twilio.com/' . $url;
+        $url = 'https://api.twilio.com' . $url;
         $curl = curl_init();
         curl_setopt_array(
             $curl,
             array(
                 CURLOPT_URL => $url,
-                CURLOPT_HTTPHEADER => array('Content-Type:application/json', 'Authorization:Bearer ' . env('16b2a6c104b60cc2695e08ec533aaa89')),
+                CURLOPT_HTTPHEADER => array('Content-Type:application/json', 'Authorization:Bearer ' . env('TWILIO_AUTH_TOKEN')),
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_POST => true,
                 CURLOPT_POSTFIELDS => json_encode($body)
@@ -40,7 +40,6 @@ class SendSMSController extends Controller
         // Your Account SID and Auth Token from twilio.com/console
         $sid = env('TWILIO_SID');
         $token = env('TWILIO_AUTH_TOKEN');
-        // $curl_post_data = new Client($sid, $token);
 
         $twilio = new Client($sid, $token);
 
@@ -50,17 +49,17 @@ class SendSMSController extends Controller
                            "body" => $request->body,
                            "from" => env('TWILIO_MOBILE_NUMBER'),
                            "statusCallback" => env('STATUS_CALLBACK').'/api/sms',
+                        //    "status" => $request->status
                            ]
                   );
-
 
 
           $url = '/2010-04-01/Accounts/AC866e1d44478f56fc29af057ee02b554b/Messages.json';
 
           Log::info('Send SMS endpoint hit');
-          Log::info($request->all());
+         Log::info($request->all());
 
-          print($curl_post_data);
+        //   print($curl_post_data);
 
 
           return $this->makeHttp($url, $curl_post_data);
